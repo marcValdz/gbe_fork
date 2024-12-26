128d149c9b167ba2ed35eccea800c9ce9c1395b6
Marc Joshua Valdez <71559949+marcValdz@users.noreply.github.com>
2024-12-26 09:08:46 +0800

    Update ach_watcher_gen.py
    *-aw* now includes a gameIndex.json that contains the necessary information for Achievement Watcher to track playtime
    
    https://www.reddit.com/r/AchievementWatcher/wiki/troubleshooting/playtime/#wiki_2._replace_the_gameindex.json_in_two_folders

--- tools/generate_emu_config/external_components/ach_watcher_gen.py
+++ tools/generate_emu_config/external_components/ach_watcher_gen.py
@@ -138,6 +138,18 @@ def generate_all_ach_watcher_schemas(
         },
         "apiVersion": 1,
     }
+    ach_watcher_gameIndex_schema = {
+        "appid": appid,
+        "name": app_name,
+        "binary": app_exe,
+        "icon": small_icon_hash,
+    }
+    # print(ach_watcher_gameIndex_schema)
+    gameIndex = []
+    gameIndex.append(ach_watcher_gameIndex_schema)
+    out_gameIndex_file = os.path.join(ach_watcher_out_dir, 'gameIndex.json')
+    with open(out_gameIndex_file, "wt", encoding='utf-8') as f:
+        json.dump(gameIndex, f, ensure_ascii=False, indent=2)
     
     langs : set[str] = set()
     for ach in achs:
